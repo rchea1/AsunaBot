a1238ff63f60bfdf3d95dc764e4a0f31c5ec6f9c
Randy Chea <rchea@ucsd.edu>
2017-10-19 02:05:07 -0700

    Handles Reddit's API

---
+++ AsunaBot/animethemes.py
@@ -0,0 +1,43 @@
+# Utilizes Reddit's API to search for openings and eventually endings for 
+# a specified anime from /r/AnimeThemes
+
+import praw 
+import config
+import re
+
+def startup():
+	print('Authenticating Reddit...')
+	reddit = praw.Reddit(config.USER, user_agent = config.user_agent)
+	return reddit
+
+# Finds the opening from /r/AnimeThemes
+# @title is the title of the anime 
+def findAnimeOpening(title):
+	reddit = startup()
+	openings = []
+
+	for submission in reddit.subreddit('animethemes').search(title, 'new'):
+		if submission.link_flair_text == 'Added to wiki' and 'OP' in submission.title:
+			openings.append(submission)
+
+	if not openings:
+		print('Could not find any openings for this anime')
+		return -1
+
+	return list(reversed(openings))
+
+# Finds the endings from /r/AnimeThemes
+# @title is the title of the anime 
+def findAnimeEnding(title):
+	reddit = startup()
+	endings = []
+
+	for submission in reddit.subreddit('animethemes').search(title, 'new'):
+		if submission.link_flair_text == 'Added to wiki' and 'ED' in submission.title:
+			endings.append(submission)
+
+	if not endings:
+		print('Could not find any openings for this anime')
+		return -1
+
+	return list(reversed(endings))
