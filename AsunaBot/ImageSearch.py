ecdb5fcd81f3e71c6bae7e77e78b1bf0dcfc6494
Randy Chea <rchea@ucsd.edu>
2017-10-29 22:32:56 -0700

    image searches for random page

--- AsunaBot/ImageSearch.py
+++ AsunaBot/ImageSearch.py
@@ -8,6 +8,7 @@ import random
 
 import xml.etree.cElementTree as ET
 from discord.ext import commands
+from math import ceil
 
 client = discord.Client()
 
@@ -21,23 +22,41 @@ class ImageSearch:
 		''' Retrieves a random image from https://konachan.com '''
 		imageArray = []
 		name = name.replace(' ', '_')
-		url = 'https://konachan.com/post.json?tags={}&limit=100'.format(name)    
-		data = requests.get(url).json()
 		
 		konachan = requests.Session()
 		request = konachan.get('https://konachan.com/post.xml?tags={}'.format(name))
 		konachan.close()
-		request = ET.fromstring(request)
-	
-		print(request)
+		request = ET.fromstring(request.content)
 
+		# konachan lists 21 images per page
+		pageNumbers = int(request.attrib['count'])
+		if pageNumbers == 0:
+			await self.bot.send_message(ctx.message.channel, 'No images were found for the tag "{}"'.format(name))
+			return
+		if pageNumbers < 21: 
+			pageNumbers = 1
+		else:
+			pageNumbers = int(ceil(pageNumbers / 21))
+
+		# Getting a random page number if there are more than 1 page of images
+		if pageNumbers > 0:
+			randomPage = random.randint(0, pageNumbers)
+			if randomPage == 0: 
+				randomPage = 1
+		else:
+			randomPage = pageNumbers
+
+		print('Getting random image of "{}" from page {}'.format(name, randomPage))
+		url = 'https://konachan.com/post.json?page={}&tags={}&limit=21'.format(randomPage, name)    
+		data = requests.get(url).json()
+	
 		for image in data:
 			info = {'image_link': 'https:' + str(image['jpeg_url']),
 					'id': image['id']}
 			imageArray.append(info)
 
-		print('Getting random image of:  ' + name)
 		image = random.choice(imageArray)
+		# Downloading the image for myself and uploading the file through discord
 		filepath = os.path.join(config.IMAGE_PATH, str(image['id']) + '.jpg')
 		urllib.request.urlretrieve(image['image_link'], filepath)
 		await self.bot.send_file(ctx.message.channel, config.IMAGE_PATH + str(image['id']) + '.jpg')
